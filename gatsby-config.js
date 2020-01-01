dc36705759c7982cc0570f5194d491b402efb60f
Jesse Pinho <jesse@jessepinho.com>
2020-01-01 08:04:51 +0100

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Don't make art!`,
+    description: `Here are all the reasons you shouldn't be making art.`,
+    author: `@jessepinho`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `dont-make-art`,
+        short_name: `dont-make-art`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
