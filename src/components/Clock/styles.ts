2dbdfbfc7c0859647f4762ce9fad9504f69c9690
Jesse Pinho <jesse@jessepinho.com>
2020-01-01 12:15:04 +0100

    Tweak the clock's appearance

--- src/components/Clock/styles.ts
+++ src/components/Clock/styles.ts
@@ -1,20 +1,18 @@
 import styled from 'styled-components'
 
 export const Section = styled.section`
-  padding-top: 5rem;
-
   color: white;
   text-align: center;
-  text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.75);
+  text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.6);
 `
 
 export const Time = styled.time`
   display: block;
-  font-size: 5rem;
+  font-size: 6rem;
   font-weight: 200;
+  margin-bottom: 0.5rem;
 `
 
 export const Date = styled.time`
   font-size: 1.5rem;
-  font-weight: 300;
 `
